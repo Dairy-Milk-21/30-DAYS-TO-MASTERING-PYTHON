8648c7cdf945422a879f105036dc79d19ffc4f5b
Abrar Daiyan <abradabra212009@gmail.com>
2025-02-20 18:56:25 +0600

    Create Day-01.py

---
+++ Day-01.py
@@ -0,0 +1,10 @@
+print("Welcome TO MY CHALLENGE. DAY-01")
+
+# Variables
+x = 21   #This is a int data type variable
+y = 21.5 #This is a Float data type variable
+z = True #This is a boolean data type variable
+print(x,y,z,sep=",") #This is a way to print multiple variables in a line
+
+# How to output any value
+print("Hello, World!") 
