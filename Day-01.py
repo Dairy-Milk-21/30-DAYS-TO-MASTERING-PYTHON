4b395637284527b8a8d9ff9c9d955fdaa916c1a8
Abrar Daiyan <abradabra212009@gmail.com>
2025-02-22 21:11:43 +0600

    Day-02

--- Day-01.py
+++ Day-01.py
@@ -4,7 +4,7 @@ print("Welcome TO MY CHALLENGE. DAY-01")
 x = 21   #This is a int data type variable
 y = 21.5 #This is a Float data type variable
 z = True #This is a boolean data type variable
-print(x,y,z,sep=",") #This is a way to print multiple variables in a line
+print(x,y,z,sep=" ") #This is a way to print multiple variables in a line
 
 # How to output any value
 print("Hello, World!") 
---
+++ Day-02.py
@@ -0,0 +1,24 @@
+#Python Inputs,Python Conditional sentence,Loops
+
+#python inputs
+x = input("Enter your name: ")
+print("Hello "+x)
+
+#Python Conditional sentence
+x = 10
+y = 20
+if x > y:
+    print("x is greater than y")
+else:
+    print("y is greater than x")
+
+#Loops
+for i in range(1,10):
+    print(i)
+
+#While loop
+i = 1
+while i < 10:
+    print(i)
+    i += 1
+    
